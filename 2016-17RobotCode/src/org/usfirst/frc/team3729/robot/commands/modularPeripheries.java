7f28ea9c2a2b3b82d523987090ac52217d0649c3
Jackson <thefriedricetaco@gmail.com>
2017-01-28 13:08:15 -0700

    I dun added the load motor. also sean did it maybe but nobody really knows...?

--- 2016-17RobotCode/src/org/usfirst/frc/team3729/robot/commands/modularPeripheries.java
+++ 2016-17RobotCode/src/org/usfirst/frc/team3729/robot/commands/modularPeripheries.java
@@ -5,13 +5,14 @@ import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.*;
 
 public class modularPeripheries {
-	Relay EatMotor1, EatMotor2;
+	Relay EatMotor1, EatMotor2, LoadMotor;
 	CANTalon ShootyMotor1, ShootyMotor2;
 	XboxControler _xbox;
 
 	public modularPeripheries(XboxControler xbox) {
 		EatMotor1 = new Relay(5);
 		EatMotor2 = new Relay(6);
+		LoadMotor = new Relay (9);
 		ShootyMotor1 = new CANTalon(7);
 		ShootyMotor2 = new CANTalon(8);
 		this._xbox = xbox;
@@ -53,9 +54,35 @@ public class modularPeripheries {
 			EatMotor1.set(Relay.Value.kOff);
 			EatMotor2.set(Relay.Value.kOff);
 		}
-
 	}
+	
+	
+	
+	//LOADING DOODLES
+		public void onOffLoading() {
+			boolean isRunning = true;
+			if (_xbox.GetX() == true && isRunning == true) {
+				isRunning = false;
+			}
+			if (_xbox.GetX() == true && isRunning == false) {
+				isRunning = true;
+			}
+			if (isRunning == true) {
+				LoadMotor.set(Relay.Value.kOn);
+			} else if (isRunning == false) {
+				LoadMotor.set(Relay.Value.kOff);
+			}
+		}
+		
+		public void conscousLoading(){
+			if(_xbox.GetX() == true){
+				LoadMotor.set(Relay.Value.kOn);
+			} else {
+				LoadMotor.set(Relay.Value.kOff);
+			}
+		}
 
+	
 	// SHOOTING DOODLES
 	public void shootButton() {
 		if (_xbox.GetA() == true) {
