6aa7f2758142e29b4953f4b375f6689f52a1fe49
RJRoboticsProgrammer <rj@rj.com>
2017-03-21 16:44:12 -0600

    Fixed merge

--- 2016-17RobotCode/src/org/usfirst/frc/team3729/robot/RobotMap.java
+++ 2016-17RobotCode/src/org/usfirst/frc/team3729/robot/RobotMap.java
@@ -1,23 +1,4 @@
 package org.usfirst.frc.team3729.robot;
-<<<<<<< HEAD
-
-/**
- * The RobotMap is a mapping from the ports sensors and actuators are wired into
- * to a variable name. This provides flexibility changing wiring, makes checking
- * the wiring easier and significantly reduces the number of magic numbers
- * floating around.
- */
-public class RobotMap {
-	// For example to map the left and right motors, you could define the
-	// following variables to use with your drivetrain subsystem.
-	// public static int leftMotor = 1;
-	// public static int rightMotor = 2;
-
-	// If you are using multiple modules, make sure to define both the port
-	// number and the module. For example you with a rangefinder:
-	// public static int rangefinderPort = 1;
-	// public static int rangefinderModule = 1;
-=======
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
  * to a variable name. This provides flexibility changing wiring, makes checking
@@ -34,5 +15,4 @@ public class RobotMap {
     // number and the module. For example you with a rangefinder:
     // public static int rangefinderPort = 1;
     // public static int rangefinderModule = 1;
->>>>>>> branch 'master' of https://github.com/RegisJesuitRobotics/Competition2017.git
 }
